1615db09cccb8c6dabb96b2818eddb62ed1cee40
Thongrob Tipsurat <thongrob.tsr@gmail.com>
2022-01-23 14:56:21 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route ,Routes} from 'react-router-dom'
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Service from './Service';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="Home" element={<Home />} />
+      <Route path="About" element={<About />} />
+      <Route path="Service" element={<Service />} />
+      <Route path="Contact" element={<Contact />} />
+    </Routes>
   );
 }
 
---
+++ src/Contact.jsx
@@ -0,0 +1,30 @@
+import MainLayout from "./layouts/MainLayout";
+function contact() {
+  return (
+    <MainLayout>
+      <div>
+        <div classname="jumbotron">
+            <h1 class="display-6">ติดต่อเรา</h1>
+        </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        blanditiis labore architecto perspiciatis aliquid, quo incidunt fugit
+        error molestiae modi aliquam voluptates rerum beatae doloremque est ut
+        quia nobis delectus non atque veniam. Explicabo sequi quae facilis
+        voluptate ut illo sunt deserunt nemo eos corporis sit ipsum enim, iure
+        incidunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        blanditiis labore architecto perspiciatis aliquid, quo incidunt fugit
+        error molestiae modi aliquam voluptates rerum beatae doloremque est ut
+        quia nobis delectus non atque veniam. Explicabo sequi quae facilis
+        voluptate ut illo sunt deserunt nemo eos corporis sit ipsum enim, iure
+        incidunt?
+      </p>
+      </div>
+    </MainLayout>
+  );
+}
+
+export default contact;
